e2cae0cdf18388ce89c84275d427956844e9f93d
Hasan Diab <113435102+HasanDiab19@users.noreply.github.com>
2023-06-18 11:53:55 +0300

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class GameFrame extends JFrame{
+    GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
